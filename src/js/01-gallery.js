8ebd520cdc155dd0646789e9f3ad453181e67f79
AcOo0 <acoo.012@gmail.com>
2023-11-03 21:33:19 +0200

    update task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
 
-console.log(galleryItems);
+
+const list = document.querySelector('.gallery');
+
+list.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+let lightbox = new SimpleLightbox(".gallery__link", {
+    captionsData: "alt",
+    captionDelay: 250,    
+});
+
+function createMarkup(arr) { 
+    return arr.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>
+</li>
+    `)
+        .join('');
+};
