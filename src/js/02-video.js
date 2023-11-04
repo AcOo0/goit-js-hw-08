a8e173e0139c2a364dab932908e9321f9ac4a8e8
AcOo0 <acoo.012@gmail.com>
2023-11-04 10:27:27 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,13 @@
-// import Player from '@vimeo/player';
-// import throttle from 'lodash/throttle';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const videoPlayer = document.querySelector('#vimeo-player');
+const player = new Player(videoPlayer);
+
+player.on('timeupdate', throttle(currentTime, 1000));
+
+function currentTime({ seconds }) { 
+    localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
